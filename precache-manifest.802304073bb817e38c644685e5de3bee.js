87bf8362b598e4f1a3eafa89262fc770fdb891f1
Elham Salamat <35340273+elham-salamat@users.noreply.github.com>
2022-04-03 03:25:48 +0200

    Updates

---
+++ precache-manifest.802304073bb817e38c644685e5de3bee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5ebbb8c5023830becbc",
+    "url": "/meet/static/css/main.5ea9dd63.chunk.css"
+  },
+  {
+    "revision": "a5ebbb8c5023830becbc",
+    "url": "/meet/static/js/main.a5ebbb8c.chunk.js"
+  },
+  {
+    "revision": "578145c6a0ab863faae1",
+    "url": "/meet/static/js/1.578145c6.chunk.js"
+  },
+  {
+    "revision": "c4d85cfae300d076fdc0",
+    "url": "/meet/static/js/2.c4d85cfa.chunk.js"
+  },
+  {
+    "revision": "ad7b9d5862399ee68b1e",
+    "url": "/meet/static/js/runtime~main.ad7b9d58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meet/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7beb743177cca8980e19bcd8529b2f47",
+    "url": "/meet/index.html"
+  }
+];
