17fd6310ea3fd3da33bf8ffb94ef2821d7438b18
Elham Salamat <35340273+elham-salamat@users.noreply.github.com>
2022-06-07 13:54:28 +0200

    data visualization added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 import EventList from './EventList';
+import EventGenre from './EventGenre';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import {extractLocations, getEvents} from './api';
@@ -53,11 +55,46 @@ class App extends Component {
     this.updateEvents(this.state.currentLocation, numberOfEvents)
   }
 
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    });
+
+    return data;
+  };
+
+
+
   render() {
+
     return (
       <div className="App">
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />        
-        <NumberOfEvents updateEventsCount={this.updateNumberOfEvents} />
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city</h4>
+        <CitySearch 
+          locations={this.state.locations} 
+          updateEvents={this.updateEvents} 
+        />        
+        <NumberOfEvents 
+          updateEventsCount={this.updateNumberOfEvents} 
+        />
+        <h4>Events in each city</h4>
+        <div className='data-vis-wrapper'>
+        <EventGenre events={this.state.events} />
+          <ResponsiveContainer height={400} >
+            <ScatterChart width={730} height={250}
+              margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis type="number" dataKey="number" name="number of events" />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>
         <EventList events={this.state.events} />
       </div>
     );
---
+++ src/EventGenre.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { PieChart, Pie, ResponsiveContainer } from 'recharts';
+
+const genres = ['React', 'JavaScript', 'Node', 'Django', 'Angular'];
+
+const EventGenre = ({ events }) => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const data = genres.map((genre) => {
+            const value = events.filter((event) => event.summary.split(' ').includes(genre)).length
+            return { name: genre, value };
+        }).filter(d => d.value > 0);
+
+        setData(data);
+
+    }, [events]);
+
+    return (
+        <ResponsiveContainer height={400}>
+            <PieChart width={400} height={400}>
+                <Pie
+                    data={data}
+                    cx={200}
+                    cy={200}
+                    label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(0)}%`}
+                    outerRadius={80}
+                    fill="#8884d8"
+                    dataKey="value"
+                >
+                </Pie>
+            </PieChart>
+        </ResponsiveContainer>
+    );
+}
+
+export default EventGenre;
